19eb895393fd5234699f5cb5310eafe6fc716423
Dmitry Sosin <Dir2@odis.su>
2026-04-05 13:27:18 +0300

    some corrections with listen location

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import fastify from 'fastify'
 const app = fastify()
 const port = 3000
+
 app.get('/', (req, res) => {
     res.send('Hello World')
-})
-app.listen( {port}, () => {
-    console.log(`Example app listening on port ${port}`)
 })
 app.get('/users', (req, res) => {
   res.send('GET /users')
@@ -14,3 +12,10 @@ app.get('/users', (req, res) => {
 app.post('/users', (req, res) => {
   res.send('POST /users')
 })
+
+
+
+app.listen( {port}, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
