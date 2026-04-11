21a94106084c7d8dec1bb49ca7da51b3717b9aac
Dmitry Sosin <Dir2@odis.su>
2026-04-11 16:30:40 +0300

    added /hello

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import fastify from 'fastify'
-const app = fastify()
+const app = fastify({
+  logger: true
+})
 const port = 3000
 
 app.get('/', (req, res) => {
@@ -13,7 +15,15 @@ app.post('/users', (req, res) => {
   res.send('POST /users')
 })
 
-
+app.get('/hello', (req, res) => {
+  const name = req.query.name
+  if(name) {
+    res.send(`Hello, ${name}!`)
+  }
+  else {
+    res.send('Hello World!')
+  }
+})
 
 app.listen( {port}, () => {
     console.log(`Example app listening on port ${port}`)
