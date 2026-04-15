4839e7af68e7476ef89acc8e829a3eaee68fa393
Dmitry Sosin <Dir2@odis.su>
2026-04-15 21:37:17 +0300

    added Pug

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
 import fastify from 'fastify'
+import view from '@fastify/view'
+import pug from 'pug'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const app = fastify({
   logger: true
 })
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+await app.register(view, { 
+  engine: { pug },
+  root: path.join(__dirname, 'views'),
+  viewExt: 'pug'
 })
+
+const state = {
+  courses: [
+    {
+      id: 1,
+      title: 'JS: Массивы',
+      description: 'Курс про массивы в JavaScript',
+    },
+    {
+      id: 2,
+      title: 'JS: Функции',
+      description: 'Курс про функции в JavaScript',
+    },
+  ],
+}
+
+app.get('/courses', (req, res) => {
+  const data = {
+    courses: state.courses,
+  }
+  res.view('courses/index', data)
+})
+
+app.get('/courses/:id', (req, res) => {
+  const { id } = req.params
+  const course = state.courses.find(({ id: courseId }) => courseId === parseInt(id))
+  
+  if (!course) {
+    res.code(404).send({ message: 'Course not found' })
+    return
+  }
+  
+  const data = { course }
+  res.view('courses/show', data)
+})
+
+app.get('/', (req, res) => {
+  res.view('index')
+})
+
 app.get('/users', (req, res) => {
   res.send('GET /users')
 })
@@ -17,15 +67,13 @@ app.post('/users', (req, res) => {
 
 app.get('/hello', (req, res) => {
   const name = req.query.name
-  if(name) {
+  if (name) {
     res.send(`Hello, ${name}!`)
-  }
-  else {
+  } else {
     res.send('Hello World!')
   }
 })
 
-app.listen( {port}, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen({ port }, () => {
+  console.log(`Example app listening on port ${port}`)
 })
-
